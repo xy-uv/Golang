165b3ffa906e8b5a907841b83ab7295699ab508f
Shahriar Ahmed ~ SHIFAT <shahriar.ahmed.7878@gmail.com>
2025-05-31 09:21:26 +0600

    setp 2 done

---
+++ 2_simple_values/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Number int
+	fmt.Println(1+9*3/2-1)
+	// string
+	fmt.Println("Hey Golang! I wanna catch you and made you!")
+	// Boolean
+	fmt.Println(true)
+	fmt.Println(false)
+	// Number floats
+	fmt.Println(1+7*3/2.0-1)
+}
