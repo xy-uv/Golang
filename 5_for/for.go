304ccc6979167d6184356025cd11ad0d2339e581
Shahriar Ahmed ~ SHIFAT <shahriar.ahmed.7878@gmail.com>
2025-06-02 23:52:58 +0600

    loop with only con for add/done

---
+++ 5_for/for.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+// for-> for is the only one constructor for lopping in go
+
+func main(){
+// while fashion type loop
+	i:=1
+	for (i<=7){
+		// fmt.Println(i)
+		i+=2
+	}
+
+// infinite loop
+	// for{
+	// 	fmt.Println("inf")
+	// }
+
+// classic for loop
+	for j:=0;j<=9;j++{
+		// fmt.Println(j)
+	}
+
+// continue/break with loop
+	for k:=0;k<=99;k+=2{
+		if (k==44){
+			// continue
+			break
+		}
+		// fmt.Println(k)
+	}
+
+// go 1.22 updated for loop with range
+	for m:=range 100{
+		fmt.Println(m)
+	}
+}
